751125aa774a9fb8bfc6b227716d1793b1b08fd9
Dolapo Sokoya <55527646+Sokoya97@users.noreply.github.com>
2020-12-15 14:17:09 +0530

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Router, Switch, Route } from "react-router-dom";
+import { Router, Switch, Route } from "react-router";
 import Login from "../components/login/login";
 import history from '../routes/history';
 import User from '../components/users/users'
