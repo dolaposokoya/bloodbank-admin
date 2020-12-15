74370e838ffcc6056bbf38b3fd654aaa3a1b527a
Dolapo Sokoya <55527646+Sokoya97@users.noreply.github.com>
2020-12-15 14:46:14 +0530

    updated

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -65,9 +65,8 @@ const Login = (props) => {
             props.LoginAction(formData, response => {
                 if (response) {
                     if (response.error === false) {
-                        return <Redirect to="/users"/>
-                        // history.push("/users");
-                        // window.location.assign('/users')
+                        // return <Redirect to="/users"/>
+                        history.push("/users");
                     }
                     else {
                         alert(response.message)
