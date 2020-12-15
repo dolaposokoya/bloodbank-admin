d57fa9aa06d78f720220d496d797eeae24d117b9
Dolapo Sokoya <55527646+Sokoya97@users.noreply.github.com>
2020-12-15 14:44:14 +0530

    new update

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux';
-import { useHistory } from 'react-router';
+import { useHistory, Redirect } from 'react-router';
 import '../../index.css'
 import {
     Avatar, Button,
@@ -65,7 +65,8 @@ const Login = (props) => {
             props.LoginAction(formData, response => {
                 if (response) {
                     if (response.error === false) {
-                        history.push("/users");
+                        return <Redirect to="/users"/>
+                        // history.push("/users");
                         // window.location.assign('/users')
                     }
                     else {
