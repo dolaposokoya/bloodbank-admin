736c70b8ea94da864ec6b14d46d09bc6a9a55812
Dolapo Sokoya <65714464+dolaposokoya@users.noreply.github.com>
2020-12-16 21:20:25 +0530

    Update login.js

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useState,useEffect } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux';
 import { useHistory } from "react-router";
